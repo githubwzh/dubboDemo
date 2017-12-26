deea06cbe99745fa2e1089e262611198c2249e76
githubwzh <772049311@qq.com>
2017-12-26 16:50:11 +0800

    dubbo+zookeeper集群

--- src/main/java/com/study/dubbo/App.java
+++ src/main/java/com/study/dubbo/App.java
@@ -9,8 +9,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App 
 {
     public static void main(String[] args) throws Exception {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                new String[] { "applicationContext.xml" });
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] { "applicationContext.xml" });
+                new String[] { "applicationContext-zookeeper-cluster.xml" });
         context.start();
         System.in.read(); // 为保证服务一直开着，利用输入流的阻塞来模拟
     }
